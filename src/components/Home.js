618192e313c4fb79e1ca9e5f212ec39f4de05a91
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-04-28 10:58:11 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 
 function Home({ isLoggedIn }) {
+  //console.log(isLoggedIn)
   if (!isLoggedIn) return <Redirect to="/login" />;
 
   return (
